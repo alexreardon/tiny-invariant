19d9db7aeacc1a3067282673ab6172cbf1c31b10
Alex Reardon <areardon@atlassian.com>
2018-04-17 14:25:08 +1000

    initial

---
+++ src/index.js
@@ -0,0 +1,22 @@
+// @flow
+
+const isProduction: boolean = process.env.NODE_ENV === 'production';
+const prefix: string = 'Invariant failed';
+
+// Throw an error if the condition fails
+// Strip out error messages for production
+export default (condition: mixed, message?: string = '') => {
+  if (condition) {
+    return;
+  }
+
+  // Condition not passed
+
+  // In production we strip the message but still throw
+  if (isProduction) {
+    throw new Error(prefix);
+  }
+
+  // In other environments we throw with the message
+  throw new Error(`${prefix}: ${message}`);
+};
