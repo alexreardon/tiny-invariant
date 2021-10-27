8c85efd48d6aaecaab672f37cfe0d6a81b1800f8
Alex Reardon <alexreardon@gmail.com>
2021-10-27 13:42:01 +1100

    continuing with older build

--- rollup.config.js
+++ rollup.config.js
@@ -5,36 +5,6 @@ import { terser } from 'rollup-plugin-terser';
 const input = 'src/tiny-invariant.ts';
 
 export default [
-  // Universal module definition (UMD) build
-  {
-    input,
-    output: {
-      file: 'dist/tiny-invariant.js',
-      format: 'umd',
-      name: 'invariant',
-    },
-    plugins: [
-      // Setting development env before running other steps
-      replace({ 'process.env.NODE_ENV': JSON.stringify('development'), preventAssignment: true }),
-      typescript({ module: 'ESNext' }),
-    ],
-  },
-  // Universal module definition (UMD) build (production)
-  {
-    input,
-    output: {
-      file: 'dist/tiny-invariant.min.js',
-      format: 'umd',
-      name: 'invariant',
-    },
-    plugins: [
-      // Setting production env before running other steps
-      replace({ 'process.env.NODE_ENV': JSON.stringify('production'), preventAssignment: true }),
-      typescript({ module: 'ESNext' }),
-      ,
-      terser(),
-    ],
-  },
   // ESM build
   {
     input,
@@ -50,8 +20,30 @@ export default [
     output: {
       file: 'dist/tiny-invariant.cjs.js',
       format: 'cjs',
-      exports: 'default',
     },
-    plugins: [typescript({ module: 'ESNext' })],
+    plugins: [typescript({ module: 'CommonJS' })],
+  },
+  // UMD: Production build
+  {
+    input,
+    output: {
+      file: 'dist/tiny-invariant.js',
+      format: 'umd',
+      name: 'invariant',
+    },
+    plugins: [typescript({ module: 'CommonJS' })],
+  },
+  {
+    input,
+    output: {
+      file: 'dist/tiny-invariant.min.js',
+      format: 'umd',
+      name: 'invariant',
+    },
+    plugins: [
+      typescript({ module: 'CommonJS' }),
+      replace({ 'process.env.NODE_ENV': JSON.stringify('production'), preventAssignment: true }),
+      terser(),
+    ],
   },
 ];
