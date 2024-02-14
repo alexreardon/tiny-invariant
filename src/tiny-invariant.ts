39666f267a483de364bbb3e73049821ab1ed9bfb
Alex Hinds <alex@optech.ai>
2024-02-14 14:57:49 +1100

    docs: typos, and explicit descriptions

--- src/tiny-invariant.ts
+++ src/tiny-invariant.ts
@@ -6,10 +6,10 @@ const prefix: string = 'Invariant failed';
  * 
  * The `invariant` function takes a value, and if the value is falsy then the `invariant` function will throw. If the value is truthy, then the function will not throw.
  *
- * @param {boolean} condition A boolean condition - if falsey will thrown an error.
+ * @param {*} condition A boolean condition - if falsy will throw an error.
  * @param {(string|() => string)} message The message provided to accompany the invariant. Can provide a string, or a function that returns a string for cases where the message takes a fair amount of effort to compute.
  *  
- * @returns {asserts condition is true}
+ * @returns {asserts condition is true} Asserts condition is truthy.
  * @example
  * ```tsx
  * import invariant from 'tiny-invariant';
